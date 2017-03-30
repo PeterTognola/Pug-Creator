14d8a03e968f81dbe60c07d8bb23e2d783eaf4c7
James Tognola <togdem1@googlemail.com>
2017-03-30 17:52:00 +0100

    Revert "Initial Project Check-In (I probably should start using Git properly...)."
    This reverts commit 2e3afdbaff347cd695a25674808ec3ed43ffc2e8.

--- PugCreator/App.xaml.cs
+++ PugCreator/App.xaml.cs
@@ -1,9 +1,17 @@
-﻿namespace PugCreator
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PugCreator
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App
+    public partial class App : Application
     {
     }
 }
